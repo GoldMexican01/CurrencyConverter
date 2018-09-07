70a978e0b7d6e0ac3b31f49366c14da1cd21c378
Ollie Pearson <Ollie170@outlook.com>
2018-09-07 11:38:27 +0100

    Actually turned it from a single variable called Dave, into an actual currency converter.

--- CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
+++ CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
@@ -1,7 +1,30 @@
 ﻿Module Module1
 
-    Sub Main()
-        Dim Dave As String
+    Sub Main(args As String())
+        Dim AmountGBP As Decimal
+        Dim Currency As String
+        Dim EUR, PLN, USD As Decimal
+        Dim Answer As Decimal
+        EUR = 1.1
+        PLN = 4.8
+        USD = 1.6
+        Console.WriteLine("Please Enter An Amount In GBP:")
+        AmountGBP = Console.ReadLine()
+        Console.WriteLine("Please Enter The Currency You Want To Convert To:")
+        Currency = Console.ReadLine()
+        If Currency = "EUR" Then
+            Answer = AmountGBP * EUR
+        ElseIf Currency = "PLN" Then
+            Answer = AmountGBP * PLN
+        ElseIf Currency = "USD" Then
+            Answer = AmountGBP * USD
+        Else
+            Answer = 0
+            Console.WriteLine("Please Enter The Three Letter Currency Code")
+        End If
+        Console.WriteLine("£ " & AmountGBP & " Is " & Answer & " " & Currency & "")
+        Console.ReadLine()
+
     End Sub
 
 End Module
