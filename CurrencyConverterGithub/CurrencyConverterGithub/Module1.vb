c5e86a95a0450ae16545644b70b1ba38d0fd54db
Ollie Pearson <Ollie170@outlook.com>
2018-09-07 13:13:34 +0100

    Added any key to exit message and invalid input message

--- CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
+++ CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
@@ -12,18 +12,19 @@
         AmountGBP = Console.ReadLine() 'This is where you enter your amount
         Console.WriteLine("Please Enter The Currency You Want To Convert To:")
         Currency = Console.ReadLine() 'Where you enter the currency (USD, EUR, PLN)
-        If Currency = "EUR" Then
+        If Currency = "EUR" Then 'If statement to determine currency input and to use the amount entered in a multiplication
             Answer = AmountGBP * EUR
         ElseIf Currency = "PLN" Then
             Answer = AmountGBP * PLN
         ElseIf Currency = "USD" Then
             Answer = AmountGBP * USD
-        Else
+        Else 'If input is invalid (its case sensitive somehow) answer is set to 0 and a message displayed
             Answer = 0
-            Console.WriteLine("Please Try Again")
+            Console.WriteLine("Please Enter The Currency's 3 Letter Code In CAPITALS")
         End If
         Console.WriteLine("£ " & AmountGBP & " Is " & Answer & " " & Currency & "")
-        Console.ReadLine()
+        Console.WriteLine("Press any key to exit...")
+        Console.ReadLine() 'Result is written and converted currency units displayed.
     End Sub
 
 End Module
