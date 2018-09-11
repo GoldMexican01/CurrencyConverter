b30ee459c7ecdd8a4ed117e7d16212a048b6661b
Ollie Pearson <Ollie170@outlook.com>
2018-09-11 10:06:09 +0100

    Fixed a spelling mistake in line 15

--- CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
+++ CurrencyConverterGithub/CurrencyConverterGithub/Module1.vb
@@ -5,14 +5,14 @@
         Dim Currency As String
         Dim EUR, PLN, USD As Decimal
         Dim Answer As Decimal
-        EUR = 1.1 'These three lines are exchane rates
+        EUR = 1.1 'These three lines are exchange rates
         PLN = 4.8
         USD = 1.6
         Console.WriteLine("Please Enter An Amount In GBP:")
         AmountGBP = Console.ReadLine() 'This is where you enter your amount
         Console.WriteLine("Please Enter The Currency You Want To Convert To:")
         Currency = Console.ReadLine() 'Where you enter the currency (USD, EUR, PLN)
-        If Currency = "EUR" Then 'If statement to determine currency input and to use the amount entered in a multiplication
+        If Currency = "EUR" Then 'If statement to determine currency input and to use the amount entered in a multiplication sum to convert to the three different currencies
             Answer = AmountGBP * EUR
         ElseIf Currency = "PLN" Then
             Answer = AmountGBP * PLN
@@ -24,7 +24,7 @@
         End If
         Console.WriteLine("£ " & AmountGBP & " Is " & Answer & " " & Currency & "")
         Console.WriteLine("Press any key to exit...")
-        Console.ReadLine() 'Result is written and converted currency units displayed.
+        Console.ReadLine()
     End Sub
 
 End Module
